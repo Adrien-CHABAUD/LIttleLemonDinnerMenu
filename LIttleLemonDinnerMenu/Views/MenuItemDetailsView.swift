7352f2e41a382d8230af759a947b017fa561e432
Adrien CHABAUD <59933787+Adrien-CHABAUD@users.noreply.github.com>
2023-08-12 01:45:41 -0400

    UI Detail page.

--- LIttleLemonDinnerMenu/Views/MenuItemDetailsView.swift
+++ LIttleLemonDinnerMenu/Views/MenuItemDetailsView.swift
@@ -13,36 +13,86 @@ struct MenuItemDetailsView: View {
     // Contains whether the page should be shown
     @Binding var showSheetView: Bool
     
+    @State private var stepperVal = 1
+    
     var body: some View {
         
         NavigationView {
             
             VStack {
-                Image("littleLemonLogo")
+                // Picture
+                Image(menuItem.picture)
                     .resizable()
-                    .frame(maxWidth: 225, maxHeight: 340)
-                Text("Price:")
+                    .frame(maxWidth: .infinity, maxHeight: 340)
+                
+                // Title
+                Text(menuItem.title)
+                    .foregroundColor(Color("Green"))
                     .fontWeight(.bold)
-                Text("$" + String(menuItem.price))
-                Text("Ordered:")
-                    .fontWeight(.bold)
-                Text("\(menuItem.orderCount)")
-                Text("Ingredients:")
-                    .fontWeight(.bold)
-                ForEach(menuItem.ingredients, id: \.self) { item in
-                    switch item {
-                    case .Broccoli:
-                        Text("Broccoli")
-                    case .Carrot:
-                        Text("Carrot")
-                    case .Pasta:
-                        Text("Pasta")
-                    case .TomatoSauce:
-                        Text("Tomato Sauce")
-                    case .Spinach:
-                        Text("Spinach")
+                    .font(.system(size: 24))
+                
+                VStack {
+                    // Description
+                    VStack {
+                        HStack {
+                            Text("Description")
+                                .foregroundColor(Color("Green"))
+                                .font(.system(size: 20))
+                            Spacer()
+                        }.padding(.bottom, 11)
+                        Text("Donec sed placerat diam. Integer dictum sed est in luctus. Sed sit amet leo congue, feugiat ante et.")
+                            .foregroundColor(Color("PaleGreen"))
+                            .font(.system(size: 15))
+                    }.padding(.vertical)
+                    
+                    // Ingredients
+                    VStack {
+                        HStack {
+                            Text("Ingredients")
+                                .foregroundColor(Color("Green"))
+                                .font(.system(size: 20))
+                            Spacer()
+                        }.padding(.bottom, 11)
+                        HStack {
+                            ForEach(menuItem.ingredients, id: \.self) { item in
+                                switch item {
+                                case .Broccoli:
+                                    Text("Broccoli")
+                                case .Carrot:
+                                    Text("Carrot")
+                                case .Pasta:
+                                    Text("Pasta")
+                                case .TomatoSauce:
+                                    Text("Tomato Sauce")
+                                case .Spinach:
+                                    Text("Spinach")
+                                }
+                            }
+                            .foregroundColor(Color("PaleGreen"))
+                            .font(.system(size: 15))
+                        }
                     }
-                }
+                    HStack {
+                        Stepper("", value: $stepperVal)
+                        Text("\(stepperVal)")
+                    }
+                    
+                    ZStack {
+                        // Back Rectangle
+                        RoundedRectangle(cornerRadius: 16)
+                            .fill(Color("Yellow"))
+                            .frame(maxWidth: 319, maxHeight: 58)
+                            .shadow(color: Color("Green"), radius: 1, x: 0, y: 4)
+                        
+                        // Front Text
+                        Text("Add 1 to cart - £10")
+                            .fontWeight(.regular)
+                            .font(.system(size: 20))
+                            .foregroundColor(.white)
+                        
+                    }.padding(.top)
+                }.padding(.horizontal)
+                
             }
             // Adding the top "Done" button to dismiss the page
             .toolbar {
@@ -50,17 +100,20 @@ struct MenuItemDetailsView: View {
                     // Allows to dismiss the sheet
                     self.showSheetView = false
                 } label: {
-                    Text("Done").bold()
+                    Text("Cancel")
+                        .bold()
+                        .foregroundColor(Color("Green"))
+                    
                 }
             }
         }
-
+        
     }
 }
 
 struct MenuItemDetailsView_Previews: PreviewProvider {
     struct MenuItemContainer: View {
-        @State private var menuItem = MenuItem(ordersCount: 20, title: "Coffee", menuCategory: .Drink, picture: "drink1", price: 10.99, ingredients: [.Broccoli], orderCount: 10)
+        @State private var menuItem = MenuItem(ordersCount: 20, title: "Coffee", menuCategory: .Drink, picture: "drink1", price: 10.99, ingredients: [.Broccoli, .Carrot], orderCount: 10)
         
         var body: some View {
             MenuItemDetailsView(menuItem: $menuItem, showSheetView: .constant(false))
